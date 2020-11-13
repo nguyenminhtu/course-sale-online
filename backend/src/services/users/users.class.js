9e6684bbe8c57886afe38687611ca5ec7311f952
nguyenminhtu <tuunguyen2795@gmail.com>
2020-11-13 21:25:41 +0700

    Fix login

--- backend/src/services/users/users.class.js
+++ backend/src/services/users/users.class.js
@@ -2,6 +2,10 @@ const { Service } = require("feathers-mongoose");
 
 exports.Users = class Users extends Service {
   async find(params) {
+    if (params.query["$limit"]) {
+      return super.find({ query: params.query });
+    }
+
     const queryParams = { $sort: { createdAt: -1 } };
 
     Object.keys(params.query).forEach((key) => {
