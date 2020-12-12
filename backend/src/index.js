32cca7ffaef75749f5df23e2111d3a159d13ba4a
nguyenminhtu <tuunguyen2795@gmail.com>
2020-12-12 18:43:40 +0700

    Finish part 1

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,13 @@
+const fs = require("fs");
+
 const logger = require("./logger");
 const app = require("./app");
 const port = app.get("port");
 const server = app.listen(port);
+
+if (!fs.existsSync(process.env.PWD + "/public/uploads")) {
+  fs.mkdirSync(process.env.PWD + "/public/uploads");
+}
 
 process.on("unhandledRejection", (reason, p) =>
   logger.error("Unhandled Rejection at: Promise ", p, reason)
